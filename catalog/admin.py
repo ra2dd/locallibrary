6865e4dcdd33cd15f6fd258f492af1635ceefa8f
ra2dd <radoslawpryw@gmail.com>
2023-05-01 21:40:03 +0200

    add Inline Instance and changed detail/list view

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,28 +5,49 @@ from .models import Genre, Language, Book, Imprint, BookInstance, Author
 
 admin.site.register(Genre)
 admin.site.register(Language)
-# admin.site.register(Book)
 admin.site.register(Imprint)
-# admin.site.register(BookInstance)
-# admin.site.register(Author)
 
 
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
+    # Display in list view
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-
+    
+    # Display in detail view
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
+# Inline BookInstance for editing it inside Book detail view
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    # book field is excluded automatically because it goes inline to book model with relationship
+    fields = ['book', 'imprint', 'status']
 
 # Register the Admin classes for Book using the decorator
 # Decorator is the function that is run before the function that you pass to decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    # Display in list view
     list_display = ('book', 'imprint', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    # Display in detail view
+    exclude = ['id']
+    fieldsets = (
+        (None, 
+        {
+            'fields': ('book', 'imprint')
+        }),
+        ('Availability',
+        {
+            'fields': ('status', 'due_back')
+        }),
+    )
