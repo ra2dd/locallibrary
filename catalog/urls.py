12de4a3770ccc20829a718b7614d456614640a3f
ra2dd <radoslawpryw@gmail.com>
2023-05-04 11:25:12 +0200

    added book list view and template

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
 
-    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail')
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     # Achieving the same thing with regular expressions
     # Learn more at https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Generic_views#url_mapping_2
-    # re_path(r'^book/(?P<pk>\d+)$'views.BookDetailView.as_view(), name='book-detail')
+    # re_path(r'^book/(?P<pk>\d+)$'views.BookDetailView.as_view(), name='book-detail'),
     
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
 ]
