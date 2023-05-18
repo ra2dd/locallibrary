462a7a06c4d89f98ab74d15b8165aee98badc663
ra2dd <98891234+ra2dd@users.noreply.github.com>
2023-05-18 16:50:15 +0200

    add additional Author models tests

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -13,18 +13,36 @@ class AuthorModelTest(TestCase):
         pass
 
     def test_first_name_label(self):
-        field_label = author.s_meta.get_field('first_name').verbose_name
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('first_name').verbose_name
         self.assertEqual(field_label, 'first name')
+
+    def test_last_name_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('last_name').verbose_name
+        self.assertEqual(field_label, 'last name')
+
+    def test_date_of_birth_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('date_of_birth').verbose_name
+        self.assertEqual(field_label, 'date of birth')
 
     def test_date_of_death_label(self):
         author = Author.objects.get(id=1)
         field_label = author._meta.get_field('date_of_death').verbose_name
         self.assertEqual(field_label, 'died')
 
+
     def test_first_name_max_length(self):
         author = Author.objects.get(id=1)
         max_length = author._meta.get_field('first_name').max_length
         self.assertEqual(max_length, 100)
+
+    def test_last_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('last_name').max_length
+        self.assertEqual(max_length, 100)
+        
     
     def test_object_name_is_first_name_last_name(self):
         author = Author.objects.get(id=1)
