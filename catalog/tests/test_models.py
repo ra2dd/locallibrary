0e6b2b54ecaefaa9675e0c5abae042f59d4f41c5
ra2dd <98891234+ra2dd@users.noreply.github.com>
2023-05-18 10:21:53 +0200

    add initial author model tests

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -1,25 +1,39 @@
 from django.test import TestCase
 
-class YourTestClass(TestCase):
+from catalog.models import Author
+
+class AuthorModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        print("setUpTestData: Run once to set up non-modified data for all class methods.")
-        pass
+        # Run once to set up non-modified used by all class methods.
+        Author.objects.create(first_name='Example', last_name="User")
         
     def setUp(self):
-        print("setUp: Run once for every test method to setup clean data")
+        # Run once for every test method to setup clean data
         pass
 
-    def test_false_is_false(self):
-         print("Method: test_false_is_false.")
-         self.assertFalse(False)
-        
-    def test_false_is_true(self):
-         print("Method: test_false_is_true.")
-         self.assertTrue(False)
+    def test_first_name_label(self):
+        field_label = author.s_meta.get_field('first_name').verbose_name
+        self.assertEqual(field_label, 'first name')
 
-    def test_one_plus_one_equals_two(self):
-         print("Method: test_one_plus_one_equals_two")
-         self.assertEqual(1 + 1, 2)
-         
+    def test_date_of_death_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('date_of_death').verbose_name
+        self.assertEqual(field_label, 'died')
+
+    def test_first_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('first_name').max_length
+        self.assertEqual(max_length, 100)
+    
+    def test_object_name_is_first_name_last_name(self):
+        author = Author.objects.get(id=1)
+        expected_object_name = f'{ author.first_name } { author.last_name }'
+        self.assertEqual(str(author), expected_object_name)
+
+    def test_get_absolute_url(self):
+        author = Author.objects.get(id=1)
+        # This will fail if the urlconf is not defined.
+        self.assertEqual(author.get_absolute_url(), '/catalog/author/1')
+
 
